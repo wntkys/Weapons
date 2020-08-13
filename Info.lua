de5bba24ddf7a76f464ede491a782bc8a9b847a7
wntkys <52929806+wntkys@users.noreply.github.com>
2020-08-13 22:45:15 +0500

    Update Info.lua

--- Info.lua
+++ Info.lua
@@ -1,9 +1,9 @@
 g_PluginInfo =
 {
 	Name = "Weapons",
-	Version = "3",
+	Version = "1",
 	Date = "2017-11-25",
-	SourceLocation = "https://github.com/mathiascode/Weapons",
+	SourceLocation = "https://github.com/wntkys/Weapons",
 	Description = [[Plugin that adds weapons to a Cuberite server. Currently available weapons are Anvil Dropper, Lightning Stick, Nuker and Sniper.]],
 
 	Commands =
