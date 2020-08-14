319c81e45eca6566b4e96f4dcbb1552204082635
wntkys <ura20439@gmail.com>
2020-08-14 14:18:05 +0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -93,10 +93,10 @@ function OnPlayerRightClick(Player, BlockX, BlockY, BlockZ, BlockFace, CursorX, 
 	elseif Weapon.m_ItemType == E_ITEM_BLAZE_ROD and Weapon.m_CustomName == "§6Nuker"  then --and not(NukerOrigin[Player:GetUniqueID])
 		World:CreateProjectile(PX, PY + 1.5, PZ, cProjectileEntity.pkSnowball, Player, Weapon, Player:GetLookVector() * 80)
 		World:BroadcastSoundEffect("entity.creeper.primed", Player:GetPosition(), 0.8, 2)
-		if NukerOrigin[ProjectileEntity:GetCreatorUniqueID()] == nil
+		if NukerOrigin[ProjectileEntity:GetCreatorUniqueID()] == nil then
 			NukerOrigin[ProjectileEntity:GetCreatorUniqueID()] = 0
 		end
-		NukerOrigin[ProjectileEntity:GetCreatorUniqueID()] = NukerOrigin[ProjectileEntity:GetCreatorUniqueID()] + 1
+		NukerOrigin[ProjectileEntity:GetCreatorUniqueID()] = NukerOrigin[ProjectileEntity:GetCreatorUniqueID()] + 1 
 		Cooldown[Player:GetUUID()] = true
 		return true
 	elseif Weapon.m_ItemType == E_ITEM_IRON_HORSE_ARMOR and Weapon.m_CustomName == "§7Sniper" then
